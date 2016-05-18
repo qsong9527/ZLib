1dbbf377fd3ef9f4aeacfcc1db660ad4e7113957
jy_zenist <407081518@qq.com>
2016-05-18 15:49:43 +0800

    完成reportlibhelper

--- io/zfile/pdf_helper.py
+++ io/zfile/pdf_helper.py
@@ -14,6 +14,10 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import inch
 from reportlab.pdfbase import pdfmetrics
 from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing, String
+from reportlab.graphics.charts.linecharts import HorizontalLineChart
+from reportlab.graphics.charts.lineplots import LinePlot
+from reportlab.graphics.charts.barcharts import BarChart
 from reportlab.pdfbase.ttfonts import TTFont
 
 
@@ -79,8 +83,8 @@ class PDFHelper():
         else:
             pdfmetrics.registerFont(TTFont('chsFont','msyh.TTF'))
 
-
     def __myFirstPage(self, canvas, doc):
+        '''定义封面页模板'''
         canvas.saveState()
         canvas.setFont("chsFont", 16)
         canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, self.TITLE)
@@ -89,23 +93,41 @@ class PDFHelper():
         canvas.restoreState()
 
     def __myLaterPage(self, canvas, doc):
+        '''定义内容页模板'''
         canvas.saveState()
         canvas.setFont("chsFont", 9)
         canvas.drawString(inch, 0.75*inch, "Copy Right @%s"% self.PAGE_INFO)
         canvas.restoreState()
 
-    def save(self):
-        self.__doc.build(self.__content, onFirstPage=self.__myFirstPage, onLaterPages=self.__myLaterPage)
-
-    def add_str_content(self, content, size=22, style=None):
+    def add_str_content(self, content, size=9, Bold=False, indent=(0.05*inch, 0.05*inch), style=None):
+        '''
+        增加文字段落
+        :param content: 段落内容文字
+        :param size: 文字大小,默认9
+        :param Bold: 是否加粗,默认False
+        :param indent: 段落前后间距,(前间距,后间距)
+        :param style: 样式文件
+        '''
         if style == None:
             style = styles["Normal"]
-        p = Paragraph(content, style)
-        # self.__content.append('<font size=%s>%s</font>' %(size, p))
+        __content = "<font name='chsFont' size=%s>%s</font>" % (size, content)
+        if Bold:
+            __content = "<b>%s</b>" % __content
+        p = Paragraph(__content, style)
+        self.__content.append(Spacer(PAGE_WIDTH, indent[0]))
         self.__content.append(p)
+        self.__content.append(Spacer(PAGE_WIDTH, indent[1]))
 
 
     def add_table_content(self, head, data, rowHeights=[30,14], colWidths=[80], style=None):
+        '''
+        添加表格
+        :param head: 表格标题(第一行)
+        :param data: 表格数据
+        :param rowHeights: 行高 默认,[30, 14*len(data)]
+        :param colWidths: 列宽 默认,[80* len(data)]
+        :param style: 样式, 默认:左对齐,上下居中,表头亮蓝色,10号中文字体,黑色0.5网格线
+        '''
         __table_data = [head]
         __table_data.append(data)
         if style == None:
@@ -118,35 +140,100 @@ class PDFHelper():
                 ('GRID',(0,0),(-1,-1),0.5,colors.black),
                 ('RIGHTPADDING',(0,0),(-1,-1),0),
             ]
-        t=Table(__table_data,style=__sty,
-               rowHeights=[rowHeights[0],].extend([rowHeights[1]]*len(data)),
-               colWidths=(lambda x: x==[80] and x.extend(x*len(head)) or x)(colWidths))  #列宽如果使用'x%'的形式来设定，首列的Log文件名不会自动换行，所以此处用hardcoded的数字
+        t=Table(__table_data,
+                rowHeights=[rowHeights[0],].extend([rowHeights[1]]*len(data)),
+                colWidths=(lambda x: x==[80] and x.extend(x*len(head)) or x)(colWidths),
+                style=__sty,
+            )
         self.__content.append(t)
 
     def add_page_break(self):
+        '''添加分页符'''
         self.__content.append(PageBreak())
 
-    def add_empty_line(self):
-        self.__content.append(Spacer(PAGE_WIDTH), 0.2*inch)
+    def add_empty_line(self, lines=1):
+        '''添加空行'''
+        self.__content.append(Spacer(PAGE_WIDTH, 0.3*inch*lines))
 
-    #TBD:
-    #1. 字体样式
-    #2. 各种图标生成: 饼图,条形图,折线图
-    #3. add_str_content 增加 字体大小选择
+    def add_lineChart(self, title, data, size=(PAGE_WIDTH-100, 300)):
+        '''
+        添加一个折线图
+        :param title: 折线图标题
+        :param data: 折线图数据
+        '''
+        __chart_width, __chart_heigh = size[0], size[1]
+        __draw = Drawing(__chart_width, __chart_heigh)
+        __draw.add(String(20, __chart_heigh-10, title, fontName="chsFont",fontSize=18, fillColor=colors.black))
 
+        lc = HorizontalLineChart()
+        lc.x, lc.y = 25, 50
+        lc.width, lc.height = __chart_width-50, __chart_heigh-100
+        lc.data = data
+        lc.joinedLines = 1
 
+        lc.valueAxis.valueMin = min(data[0])
+        lc.valueAxis.valueMax = max(data[0])
+        valueRange = lc.valueAxis.valueMax - lc.valueAxis.valueMin
+        lc.valueAxis.valueStep = valueRange / 10.0
+
+        __draw.add(lc)
+        self.__content.append(__draw)
+
+    def add_linePlotChart(self, title, data, size=(PAGE_WIDTH-100, 300)):
+
+        __chart_width, __chart_heigh = size[0], size[1]
+        __draw = Drawing(__chart_width, __chart_heigh)
+        __draw.add(String(20, __chart_heigh-10, title, fontName="chsFont",fontSize=18, fillColor=colors.black))
+        #LinePlot基本属性
+        lp = LinePlot()
+        lp.x, lp.y = 25, 50
+        lp.width, lp.height = __chart_width-50, __chart_heigh-100
+        lp.data = data
+        lp.joinedLines = 1
+        #X轴配置
+        lp.xValueAxis.valueMin = min([x[0] for x in data[0]])
+        lp.xValueAxis.valueMax = max([x[0] for x in data[0]])
+        valueRange = lp.xValueAxis.valueMax - lp.xValueAxis.valueMin
+        lp.xValueAxis.valueStep = valueRange / 10.0
+        #Y轴配置
+        yValueMin = min([x[1] for x in data[0]])
+        yValueMax = max([x[1] for x in data[0]])
+        yValueRange = yValueMax - yValueMin
+        if (yValueMin-yValueRange/2) > 0:
+            lp.yValueAxis.valueMin = yValueMin - yValueRange/2
+        else:
+            lp.yValueAxis.valueMin = yValueMin
+        lp.yValueAxis.valueMax = yValueMax+ 0.01 + yValueRange/3 #+1避免当y轴最大值和最小值相等时valuseStep为0,导致绘图库计算y轴格数时出现除零错误
+        lp.yValueAxis.valueStep = (lp.yValueAxis.valueMax - lp.yValueAxis.valueMin)/10
+        lp.yValueAxis.visibleGrid = 1
+        lp.yValueAxis.gridStrokeWidth = 0.5
+        lp.yValueAxis.gridStrokeColor = colors.gray
+        __draw.add(lp)
+        self.__content.append(__draw)
+
+    def build(self):
+        '''生成PDF文件'''
+        self.__doc.build(self.__content, onFirstPage=self.__myFirstPage, onLaterPages=self.__myLaterPage)
 
 
 
 if __name__ == '__main__':
     pdf = PDFHelper('demo.pdf')
-    pdf.add_str_content("Test Demo")
+    pdf.add_str_content("俊毅的PDF测试Demo")
+    pdf.add_str_content("俊毅的PDF测试Demo", Bold=True)
+    pdf.add_str_content("俊毅的PDF测试Demo", size=24)
+    pdf.add_empty_line(4)
+    data = [[1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,6,7,8,9]]
+    pdf.add_lineChart("测试折线图", data)
+    pdf.add_empty_line(4)
+    data = [[(1,2),(2,3),(3,4),(4,2),(5,1),(6,4),(7,2),(8,3),(9,7),(10,5)]]
+    pdf.add_linePlotChart("测试折线图", data)
     pdf.add_page_break()
     pdf.add_str_content("Table Demo")
     __table_head = ["Item","Author", "CreateDate"]
     __table_date = ["Python Reportlib Demo","jy.zenist.song", "2016-05-17"]
     pdf.add_table_content(__table_head,__table_date, colWidths=[360,100,100])
-    pdf.save()
+    pdf.build()
 
 
 
--- io/zfile/test/test_pdf.py
+++ io/zfile/test/test_pdf.py
@@ -9,9 +9,6 @@
 
 import unittest
 
-from reportlab.platypus import *
-from io.zfile.pdf_helper import PDFHelper
-
 
 class TestReportUserGuide(unittest.TestCase):
 
@@ -125,14 +122,13 @@ class TestReportUserGuide(unittest.TestCase):
         c.showPage()
         c.save()
 
-    def test_08_forms(self):
+    def test_08_table(self):
         from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph, Table
         from reportlab.lib.units import inch
         from reportlab.lib.enums import TA_JUSTIFY
         from reportlab.pdfbase import pdfmetrics
         from reportlab.pdfbase.ttfonts import TTFont
-        from reportlab.pdfgen import canvas
         from reportlab.lib import fonts, colors
 
         pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
@@ -155,6 +151,237 @@ class TestReportUserGuide(unittest.TestCase):
         elements.append(table)
 
         doc.build(elements)
+
+    def test_09_docTemplate(self):
+        from reportlab.lib.styles import getSampleStyleSheet
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph
+        from reportlab.pdfbase import pdfmetrics
+        from reportlab.pdfbase.ttfonts import TTFont
+
+        pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
+        stylesheet = getSampleStyleSheet()
+
+        elements = []
+        doc = SimpleDocTemplate("demo.pdf")
+
+        elements.append(Paragraph('<font name="chsFont">AUT OOM测试报告</font>', stylesheet['Title']))
+        elements.append(Spacer(1,12))
+
+        doc.build(elements)
+
+    def test_11_drawing(self):
+        from reportlab.lib.styles import getSampleStyleSheet
+        from reportlab.lib import colors
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph
+        from reportlab.pdfbase import pdfmetrics
+        from reportlab.graphics.shapes import Drawing, Rect
+        from reportlab.pdfbase.ttfonts import TTFont
+
+        pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
+        stylesheet = getSampleStyleSheet()
+
+        elements = []
+        doc = SimpleDocTemplate("demo.pdf")
+
+        elements.append(Paragraph('<font name="chsFont">JY.zenist.song - 俊毅</font>', stylesheet['Title']))
+        elements.append(Spacer(1,12))
+
+        d = Drawing(400,200)
+        d.add(Rect(50,50,300,100, fillColor=colors.yellow))
+        elements.append(d)
+
+        doc.build(elements)
+
+
+    def test_12_drawingRenderPDF(self):
+        from reportlab.lib import colors
+        from reportlab.graphics.shapes import Drawing, Rect
+        from reportlab.graphics import renderPDF
+
+        d = Drawing(400,200)
+        d.add(Rect(50,50,300,100, fillColor=colors.yellow))
+        renderPDF.drawToFile(d, 'demo.pdf', 'JY.zenist.song')
+
+    def test_13_drawingRenderPNG(self):
+        from reportlab.lib import colors
+        from reportlab.graphics.shapes import Drawing, Rect
+        from reportlab.graphics import renderPM
+
+        d = Drawing(400,200)
+        d.add(Rect(50,50,300,100, fillColor=colors.yellow))
+        renderPM.drawToFile(d, 'demo.jpg','JPG')
+
+    def test_21_barCharts(self):
+        from reportlab.lib.styles import getSampleStyleSheet
+        from reportlab.lib import colors
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph
+        from reportlab.pdfbase import pdfmetrics
+        from reportlab.graphics.shapes import Drawing
+        from reportlab.pdfbase.ttfonts import TTFont
+        from reportlab.graphics.charts.barcharts import VerticalBarChart
+
+        pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
+        stylesheet = getSampleStyleSheet()
+
+        elements = []
+        doc = SimpleDocTemplate("demo.pdf")
+
+        elements.append(Paragraph('<font name="chsFont">JY.zenist.song - 俊毅</font>', stylesheet['Title']))
+        elements.append(Spacer(1,12))
+
+        d = Drawing(400,200)
+        data = [
+            (13,5,20,22,37,45,19,4),
+            (14,6,21,23,38,46,20,5)
+        ]
+        bc = VerticalBarChart()
+        bc.x = 50
+        bc.y = 50
+        bc.height = 125
+        bc.width = 300
+        bc.data = data
+        bc.strokeColor = colors.black
+        bc.groupSpacing = 10
+        bc.barSpacing = 2.5
+
+        bc.valueAxis._valueMin = 0
+        bc.valueAxis._valueMax = 50
+        bc.valueAxis._valueStep = 10
+
+        bc.categoryAxis.categoryNames = ['1','2','3','4','5','6','7','8']
+        # bc.categoryAxis.style = 'stacked'
+        d.add(bc)
+
+        elements.append(d)
+
+        doc.build(elements)
+
+    def test_22_lineCharts(self):
+        from reportlab.lib.styles import getSampleStyleSheet
+        from reportlab.lib import colors
+        from reportlab.lib.units import inch
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph
+        from reportlab.pdfbase import pdfmetrics
+        from reportlab.graphics.shapes import Drawing
+        from reportlab.pdfbase.ttfonts import TTFont
+        from reportlab.graphics.charts.linecharts import HorizontalLineChart
+        import random
+
+        pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
+        stylesheet = getSampleStyleSheet()
+
+        elements = []
+        doc = SimpleDocTemplate("demo.pdf")
+
+        elements.append(Paragraph('<font name="chsFont">JY.zenist.song - 俊毅</font>', stylesheet['Title']))
+        elements.append(Spacer(1,1*inch))
+
+        d = Drawing(400,200)
+        data = [
+            [13,5,20,22,37,45,19,4],
+            # [14,6,21,23,38,46,20,5]
+        ]
+        for i in range(500):
+            data[0].append(random.randint(1,100))
+            # data[1].append(random.randint(1,100))
+        lc = HorizontalLineChart()
+        lc.x = 50
+        lc.y = 50
+        lc.height = 180
+        lc.width = 300
+        lc.data = data
+        lc.joinedLines = 1
+        # lc.categoryAxis.categoryNames = data[0]
+        d.add(lc)
+
+        elements.append(d)
+
+        doc.build(elements)
+
+    def test_23_linePlotCharts(self):
+        from reportlab.lib.styles import getSampleStyleSheet
+        from reportlab.lib import colors
+        from reportlab.lib.units import inch
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph
+        from reportlab.pdfbase import pdfmetrics
+        from reportlab.graphics.shapes import Drawing
+        from reportlab.pdfbase.ttfonts import TTFont
+        from reportlab.graphics.charts.lineplots import LinePlot
+        from reportlab.graphics.widgets.markers import makeMarker
+
+        pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
+        stylesheet = getSampleStyleSheet()
+
+        elements = []
+        doc = SimpleDocTemplate("demo.pdf")
+
+        elements.append(Paragraph('<font name="chsFont">JY.zenist.song - 俊毅</font>', stylesheet['Title']))
+        elements.append(Spacer(1,1*inch))
+
+        d = Drawing(400,200)
+        data = [
+            [(1,1), (2,2), (2.5,1), (3,3), (4,5)],
+        ]
+        lp = LinePlot()
+        lp.x = 5
+        lp.y = 5
+        lp.height = 190
+        lp.width = 390
+        lp.data = data
+        lp.joinedLines = 1
+        lp.lines[0].symbol = makeMarker('FilledCircle')
+        d.add(lp)
+
+        elements.append(d)
+
+        doc.build(elements)
+
+    def test_24_PieCharts(self):
+        from reportlab.lib.styles import getSampleStyleSheet
+        from reportlab.lib import colors
+        from reportlab.lib.units import inch
+        from reportlab.platypus import SimpleDocTemplate, Spacer, Paragraph
+        from reportlab.pdfbase import pdfmetrics
+        from reportlab.graphics.shapes import Drawing
+        from reportlab.pdfbase.ttfonts import TTFont
+        from reportlab.graphics.charts.piecharts import Pie
+
+        pdfmetrics.registerFont(TTFont('chsFont', 'STHeiti Light.ttc'))
+        stylesheet = getSampleStyleSheet()
+
+        elements = []
+        doc = SimpleDocTemplate("demo.pdf")
+
+        elements.append(Paragraph('<font name="chsFont">JY.zenist.song - 俊毅</font>', stylesheet['Title']))
+        elements.append(Spacer(1,1*inch))
+
+        d = Drawing(400,200)
+        data = [13,5,20,22,37,45]
+        pc = Pie()
+        pc.x = 65
+        pc.y = 15
+        pc.width = 150
+        pc.height = 150
+        pc.data = data
+        pc.labels = ['a','b','c','d','e','f']
+        d.add(pc)
+
+        elements.append(d)
+
+        doc.build(elements)
+
+    def test_list_extend(self):
+        import random
+        __list = []
+        for i in range(60000):
+            __list.append(random.randint(0,100))
+        print __list
+
+    def test_list_min(self):
+        data = [[6,2,3,4,5]]
+        print min(data[0])
+
+
 
 
 
